7ce3cbca38a41be344e2f9ac3f506abd18737ed7
Vincent Bryan F. Calija <vincentbryan.c@agentsofvalue.com>
2020-06-18 11:43:37 +0800

    simple home and about navigation

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Navigation from './components/Navigation';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Hello World!
-        </p>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div>
+        <Navigation />
+          <Switch>
+            <Route path="/" component={Home} exact/>
+            <Route path="/about" component={About}/>
+          </Switch>
+      </div> 
+    </BrowserRouter>
   );
 }
 
